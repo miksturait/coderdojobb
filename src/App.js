a8bef14f10a00c434cef8bf01a0825d2cd5579d8
Dawid Holisz <dawid.holisz@gmail.com>
2020-01-23 19:06:41 +0100

    Create component for meeting

--- src/App.js
+++ src/App.js
@@ -2,25 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import Calendar from './pages/Calendar'
-import calendar from './data/dataMock.json'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Calendar/>
     </div>
   );
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-const Calendar = () => {
-  return (
-    <div>
-      <h1>Calendar page!! </h1>
-    </div>
-  )
+import Meeting from '../components/Meeting'
+import { calendar } from '../data/dataMock.json'
+
+class Calendar extends React.Component {
+  state = {
+    filteredMeetings: calendar
+  }
+
+  filterMeetings = event => {
+    const inputValue = event.target.value
+
+    const filteredMeetings = calendar.filter(meeting => meeting.topic.includes(inputValue))
+    this.setState({
+      filteredMeetings
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <input onChange={this.filterMeetings}/>
+        <h1>Calendar page!! </h1>
+        {this.state.filteredMeetings.map((meeting, index) => <Meeting {...meeting} key={index}/>)}
+      </div>
+    )
+  }
 }
 
 export default Calendar
