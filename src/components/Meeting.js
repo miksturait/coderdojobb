7800674a04ac725a111e2922ff175910aaec9517
Lukasz Walczak <lukeabove@gmail.com>
2020-01-24 13:52:26 +0100

    Add date formatting

--- src/components/Meeting.js
+++ src/components/Meeting.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
+import moment from 'moment';
 
 class Meeting extends Component {
   render () {
 
     const { date, topic, message, hosts, isTakingPlace } = this.props
-
+    const formattedDate = moment(date).format('DD-MMMM-YYYY')
+    const now = moment(new Date())
+    const daysToMeeting = now.to(date)
+    
     return (
       <div>
         <h2>
-          {topic}
+          {topic} will start in {daysToMeeting}
         </h2>
         <small>
-          {date}
+          {formattedDate}
         </small>
         <p>
           {message}
