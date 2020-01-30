2a5dd1a8b76711a659200ccf776ef23061098c04
Przemyslaw Krawczyk <przemekk437@gmail.com>
2020-01-30 18:30:32 +0100

    Added extra property for state to hold data from API

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,50 +1,56 @@
+<<<<<<< HEAD
 import React from 'react';
 
 import Meeting from '../components/Meeting';
 import { fetchCalendar } from '../client';
+=======
+import React from 'react'
+import Meeting from '../components/Meeting'
+import axios from 'axios'
+
+const url = "http://localhost:3000/calendar";
+>>>>>>> Added extra property for state to hold data from API
 
 class Calendar extends React.Component {
   state = {
     filteredMeetings: [],
-  };
-
-  componentDidMount() {
-    fetchCalendar().then(({ data }) => this.setState(() => ({ filteredMeetings: data })));
+    allMeetings:[]
   }
 
-  updateState = data => {
-    this.setState(() => ({ filteredMeetings: data }));
-  };
-
+  componentDidMount() {
+    axios.get(url)
+    .then(res => {
+      const allMeetings = res.data;
+      this.setState({
+        allMeetings,
+        filteredMeetings: allMeetings
+      })
+    })
+  }
   filterMeetings = event => {
-    const inputValue = event.target.value;
-
-    const filteredMeetings = this.state.filteredMeetings.filter(
-      meeting => meeting.topic.includes(inputValue) || meeting.date.includes(inputValue),
-    );
-    this.setState({
-      filteredMeetings,
-    });
-  };
-
-  filterByDate = e => {
-    const inputSelectedDate = e.target.value;
-    const filteredMeetings = calendar.filter(meeting => meeting.date.includes(inputSelectedDate))
+    const inputValue = event.target.value
+    const filteredMeetings = this.state.allMeetings.filter(meeting => meeting.topic.includes(inputValue))
     this.setState({
       filteredMeetings
     })
   }
-
+  filterDate = event => {
+    const inputValue = event.target.value
+    const filteredMeetings = this.state.allMeetings.filter(meeting => meeting.date.includes(inputValue))
+    this.setState({
+      filteredMeetings
+    })
+  }
   render () {
+    console.log(this.state.filteredMeetings)
     return (
       <div>
-        <input onChange={this.filterMeetings}/>
-        <input type="date" onChange={this.filterByDate}/>
+        <input onChange={this.filterMeetings}/><br></br>
+        data<input onChange={this.filterDate}></input>
         <h1>Calendar page!! </h1>
         {this.state.filteredMeetings.map((meeting, index) => <Meeting {...meeting} key={index}/>)}
       </div>
     );
   }
 }
-
-export default Calendar;
+export default Calendar
