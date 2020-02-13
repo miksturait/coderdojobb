e39e763019232ccbe80b8f7716befc71a9adbb2c
Jan Gajdowicz <jan.gajdowicz@gmail.com>
2020-02-13 19:08:52 +0100

    Add basic styling

--- src/components/Meeting.js
+++ src/components/Meeting.js
@@ -6,21 +6,32 @@ class Meeting extends Component {
     const { date, topic, message, hosts, isTakingPlace } = this.props;
 
     const formattedDate = moment(date).format('DD/MM/YYYY HH:mm');
+    const statusClass = isTakingPlace ? 'active' : 'inactive' 
 
     return (
-      <div>
-        <h2>{topic}</h2>
-        <small>{formattedDate}</small>
-        <p>{message}</p>
-        <ol>
-          {hosts.map((host, index) => (
-            <li key={index}>{host}</li>
-          ))}
-        </ol>
-        <label htmlFor="is-taking-place">
-          {isTakingPlace ? 'Meeting will take place' : 'Meeting will not take place'}
-          <input type="checkbox" name="is-taking-place" disabled checked={isTakingPlace} />
-        </label>
+      <div className="meeting">
+        <div className="meeting__container">
+          <div className="meeting__header">
+            <h2>{topic}</h2>
+          </div>
+          <div className="meeting__date">{formattedDate}</div>
+          <div className="meeting__message">
+            <p>{message}</p>
+          </div>
+          <div className="meeting__hosts">
+            <ol>
+            {hosts.map((host, index) => (
+              <li key={index}>{host}</li>
+              ))}
+            </ol>
+          </div>
+          <div className={`meeting__status ${statusClass}`}>
+            <label htmlFor="is-taking-place">
+              {isTakingPlace ? 'Meeting will take place' : 'Meeting will not take place'}
+              <input className="meeting__checkbox" type="checkbox" name="is-taking-place" disabled checked={isTakingPlace} />
+            </label>
+          </div>
+        </div>
       </div>
     );
   }
