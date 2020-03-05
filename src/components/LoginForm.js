1a7af65927147e615d137beb26d5fabe1f2d0e85
JKania <jacek.kafel.kania@gmail.com>
2020-03-05 17:15:18 +0100

    backuping

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function LoginForm(){
+    return (
+        <>
+        <form>
+            <input type="text" placeholder="Enter email" />>
+            <input type="text" placeholder="Enter email" />>
+            <button  />>
+        </form>
+        </>
+    )
+}
